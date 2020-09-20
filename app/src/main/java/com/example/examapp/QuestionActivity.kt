5da8a1e2c6684c1679f9f91803da006be9c3bffa
jia.jiuan.tsai <jia.jiuan.tsai@iths.se>
2020-09-20 21:49:05 +0200

    Answers are now randomised on Buttons and able to identify correct and wrong answers.

--- app/src/main/java/com/example/examapp/QuestionActivity.kt
+++ app/src/main/java/com/example/examapp/QuestionActivity.kt
@@ -3,9 +3,12 @@ package com.example.examapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import java.util.*
+import kotlin.random.Random.Default.nextInt
 
 class QuestionActivity : AppCompatActivity() {
 
@@ -14,6 +17,15 @@ class QuestionActivity : AppCompatActivity() {
     lateinit var button1: Button
     lateinit var button2: Button
     lateinit var button3: Button
+    var locationArray = mutableListOf<Int>(0,0,0,0)
+
+fun checkAnswer (view: View){
+    if (locationArray[0].toString() == view.tag.toString()){
+        Toast.makeText(this,"Correct!", Toast.LENGTH_SHORT).show()
+    }else {
+        Toast.makeText(this,"Wrong!", Toast.LENGTH_SHORT).show()
+    }
+}
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,19 +39,49 @@ class QuestionActivity : AppCompatActivity() {
         button3 = findViewById(R.id.button3)
 
         var currentCountry = intent.getStringExtra("currentCountry")
-        var delimiter =","
+        var delimiter = ","
         var splitTag = currentCountry!!.split(delimiter)
 
         //Log.d("!!!!", splitTag.toString())
 
+        locationArray = mutableListOf<Int>(0,0,0,0)
+        locationArray[0] = Random().nextInt(4)
+        locationArray[1] = Random().nextInt(4)
+        locationArray[2] = Random().nextInt(4)
+        locationArray[3] = Random().nextInt(4)
+        while (locationArray[1] == locationArray[0]) {
+            locationArray[1] = Random().nextInt(4)
+        }
+        while (locationArray[2] == locationArray[0] || locationArray[2] == locationArray[1]) {
+            locationArray[2] = Random().nextInt(4)
+        }
+        while (locationArray[3] == locationArray[0] || locationArray[3] == locationArray[1] || locationArray[3] == locationArray[2]) {
+            locationArray[3] = Random().nextInt(4)
+        }
+
+
+        var buttonTextList = mutableListOf<String>("","","","")
+
+
+        for (i in 0..3) {
+            buttonTextList[locationArray[i]] = splitTag[i + 1]
+
+        }
+
+
         currentCountryTV.setText(splitTag[0])
-        button0.setText(splitTag[1])
-        button1.setText(splitTag[2])
-        button2.setText(splitTag[3])
-        button3.setText(splitTag[4])
+        button0.setText(buttonTextList[0])
+        button1.setText(buttonTextList[1])
+        button2.setText(buttonTextList[2])
+        button3.setText(buttonTextList[3])
+
+
+
+
+
+    }
 
 
 
         //Toast.makeText(this, currentCountry, Toast.LENGTH_LONG).show()
     }
-}
